7933ba8482a46f66d243164aeb6b6301cb989ae8
xumeng <xm_2048@qq.com>
2024-05-13 23:35:27 +0800

    update

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/algebra.c.obj"
+  "CMakeFiles/main.dir/src/algebra.c.obj.d"
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+  "D:/code/mygit/xumeng-hw1/bin/libmain.dll.a"
+  "D:/code/mygit/xumeng-hw1/bin/main.exe"
+  "D:/code/mygit/xumeng-hw1/bin/main.exe.manifest"
+  "D:/code/mygit/xumeng-hw1/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/main.dir/src/algebra.c.obj.d
@@ -0,0 +1,17 @@
+CMakeFiles/main.dir/src/algebra.c.obj: \
+ D:\code\mygit\xumeng-hw1\src\algebra.c \
+ D:/code/mygit/xumeng-hw1/inc/algebra.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ C:/Users/xxguo/Downloads/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/x86_64-w64-mingw32/include/math.h
