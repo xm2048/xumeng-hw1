c2b4be631eb0a761d1901a87cfbaffbb6cf26b89
xumeng <xm_2048@qq.com>
2024-05-14 15:18:10 +0800

    first commit

--- src/algebra.c
+++ src/algebra.c
@@ -2,7 +2,7 @@
  * @Author: xumeng xm_2048@qq.com
  * @Date: 2024-05-13 22:22:23
  * @LastEditors: xumeng xm_2048@qq.com
- * @LastEditTime: 2024-05-14 15:07:13
+ * @LastEditTime: 2024-05-14 15:14:03
  * @FilePath: \xumeng-hw1\src\algebra.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
