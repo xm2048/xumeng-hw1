967a346e92d243d2855b3490e081e238f993ee43
xumeng <xm_2048@qq.com>
2024-05-19 21:24:21 +0800

    first commit

--- src/algebra.c
+++ src/algebra.c
@@ -1,5 +1,13 @@
 /*
  * @Author: xumeng xm_2048@qq.com
+ * @Date: 2024-05-19 19:37:30
+ * @LastEditors: xumeng xm_2048@qq.com
+ * @LastEditTime: 2024-05-19 21:23:16
+ * @FilePath: \xumeng-hw1\src\algebra.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: xumeng xm_2048@qq.com
  * @Date: 2024-05-13 22:22:23
  * @LastEditors: xumeng xm_2048@qq.com
  * @LastEditTime: 2024-05-19 21:20:07
@@ -9,6 +17,7 @@
 #include "algebra.h"
 #include <stdio.h>
 #include <math.h>
+Matrix adj_matrix(Matrix a);
 
 Matrix create_matrix(int row, int col)
 {
@@ -162,6 +171,7 @@ Matrix inv_matrix(Matrix a) {
 
 
 //求一个矩阵的伴随矩阵
+
 Matrix adj_matrix(Matrix a) {
     if (a.rows != a.cols) {
         printf("Error: The matrix must be a square matrix.\n");
